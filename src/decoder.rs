3cafb9f833f039bdde4f42b35a628e105642920d
Konkitoman <konkitoman@users.noreply.github.com>
2023-06-04 23:30:27 +0300

    Trying to understand

--- src/decoder.rs
+++ src/decoder.rs
@@ -38,16 +38,17 @@ impl Frame {
             crate::frame_packing::FramePacking::OneFrame(size) => {
                 let mut data = packet.data.drain(..size).collect::<Vec<u8>>();
 
-                fn f(i: usize) -> u16 {
-                    1
+                let mut f = [0; u16::MAX as usize];
+                for byte in data.iter() {
+                    f[*byte as usize] += 1;
                 }
                 // n -1 this is an non inclusive range so n
-                let ft: u16 = (0..size).map(|i| f(i)).sum();
-                fn fl(k: usize) -> u16 {
-                    (0..k).map(|i| f(i)).sum::<u16>()
+                let ft: u16 = (0..size).map(|i| f[i]).sum();
+                fn fl(f: &[u16], k: usize) -> u16 {
+                    (0..k).map(|i| f[i]).sum::<u16>()
                 }
-                fn fh(k: usize) -> u16 {
-                    fl(k) + f(k)
+                fn fh(f: &[u16], k: usize) -> u16 {
+                    fl(f, k) + f[k]
                 };
 
                 let b0 = data.drain(..1).next().unwrap();
@@ -56,7 +57,22 @@ impl Frame {
                 let mut val = 127u32 - (b0 >> 1) as u32;
                 let mut rng = 128u32;
 
-                let fs = ft as u32 - (val / (rng / ft as u32) + 1).min(ft as u32);
+                let fs = ft as u32 - (val / ((rng / ft as u32) + 1)).min(ft as u32);
+                let val = val - (rng / ft as u32) * (ft - fh(&f, b0 as usize)) as u32;
+                let rng = if fl(&f, b0 as usize) > 0 {
+                    rng / ft as u32 * (fh(&f, b0 as usize) as u32 - fl(&f, b0 as usize) as u32)
+                } else {
+                    (rng - (rng / ft as u32)) * ft as u32 - fh(&f, b0 as usize) as u32
+                };
+
+                if normalization {
+                    if rng < 8388608 {
+                        let rng = rng << 8;
+                        let sym = data.drain(..1).next().unwrap() as u32;
+                        println!("Sym: {sym}");
+                        unimplemented!("Read: 4.1.2.1");
+                    }
+                }
 
                 println!("Val: {val}, rng: {rng}, normalization: {normalization}, fs: {fs}");
             }
